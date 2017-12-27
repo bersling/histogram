56e1eefe4cda04c651c7150ecc4772b06d1211d9
bersling <bersling@gmail.com>
2017-12-27 18:25:34 +0100

    base version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+
+  data = [
+    {size: 0.1, img: 'bla.png'},
+    {size: 0.4, img: 'bla.png'},
+    {size: 1.3, img: 'bla.png'}
+  ];
+
+  numberBins: number;
+  start: number;
+  end: number;
+  bins;
+
+  setBins() {
+    this.bins = [];
+    const binLength = (this.end - this.start) / this.numberBins;
+    for (let nthBin = 0; nthBin < this.numberBins; nthBin++) {
+      const bin = this.data
+        .filter(dataPoint => dataPoint.size >= nthBin * binLength && dataPoint.size < (nthBin + 1) * binLength);
+      this.bins.push(bin);
+    }
+  }
+
 }
